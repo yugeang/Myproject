f1a45e4ddd973bc64742d3969efdccc70f9a58f7
Winebug <ydjoo0410@gmail.com>
2025-06-30 17:19:18 +0900

    temp.cs
    modify temp.cs

---
+++ Assets/temp.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.WriteLine("hello");
+		//add
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
